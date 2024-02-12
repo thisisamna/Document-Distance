d8cb297dc4013cba23bcf0d66a1e98b2f8d32cca
Amna Ahmed <ammy5342@gmail.com>
2024-02-12 22:59:32 +0200

    Read files into vectors

--- DocumentDistance/DocDistance.cs
+++ DocumentDistance/DocDistance.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+
 using System.Threading.Tasks;
 
 namespace DocumentDistance
@@ -22,6 +24,35 @@ namespace DocumentDistance
         public static double CalculateDistance(string doc1FilePath, string doc2FilePath)
         {
             // TODO comment the following line THEN fill your code here
+            string[] doc = new string[2];
+
+            doc[0] = File.ReadAllText(doc1FilePath);
+            doc[1]= File.ReadAllText(doc2FilePath);
+
+            Dictionary<string, int>[] vector = new Dictionary<string, int>[2];
+            vector[0] = new Dictionary<string, int>();
+            vector[1] = new Dictionary<string, int>();
+            string word;
+            Regex regex = new Regex("(A-Za-z0-9)*");
+            for (int i = 0; i < 2; i++)
+            {
+                MatchCollection matches = regex.Matches(doc[i]);
+
+                foreach (Match result in matches)
+                {
+                    word = result.ToString();
+                    if(vector[i].ContainsKey(word))
+                    {
+                        vector[i][result.ToString()] += 1;
+                    }
+                    else
+                    {
+                        vector[i][result.ToString()] = 1;
+
+                    }
+                }
+            }
+
             throw new NotImplementedException();
         }
     }
