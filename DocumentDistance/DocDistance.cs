2a19d1ff0dd4caf20cc3e8c61c94a3b69d70e217
Amna Ahmed <ammy5342@gmail.com>
2024-02-14 23:16:04 +0200

    Extracted parse document method to prepare for multithreading

--- DocumentDistance/DocDistance.cs
+++ DocumentDistance/DocDistance.cs
@@ -21,68 +21,17 @@ namespace DocumentDistance
         /// <param name="doc1FilePath">File path of 1st document</param>
         /// <param name="doc2FilePath">File path of 2nd document</param>
         /// <returns>The angle (in degree) between the 2 documents</returns>
+        private static Dictionary<string, int[]> vectors;
         public static double CalculateDistance(string doc1FilePath, string doc2FilePath)
         {
             // TODO comment the following line THEN fill your code here
-            string[] doc = new string[2];
-
-            doc[0] = File.ReadAllText(doc1FilePath);
-            doc[1]= File.ReadAllText(doc2FilePath);
-
-            Dictionary<string, int[]> vectors = new Dictionary<string, int[]>();
-
-            string word; char letter;
-            for (int d = 0; d < 2; d++)
-            {
-                int length = doc[d].Length;
-                word = "";
-
-                for (int i = 0; i < length; i++)
-                {
-                    letter = doc[d][i];
-                    if (Char.IsLetterOrDigit(letter))
-                    {
-                        word += letter;
-                    }
-                    else if (word.Length>0)
-                    {
-                        word = word.ToLower();
-                         
-                        if (vectors.ContainsKey(word))
-                        {
-                            vectors[word][d] += 1;
-                        }
-                        else
-                        {
-                            vectors[word] = new int[2];
-                            vectors[word][d] = 1;
-                            //vectors[word][i+1%2] = 0;
+            vectors = new Dictionary<string, int[]>();
 
 
-                        }
-                        word = "";
-
-                    }
-                }
-               if (word.Length > 0)
-                {
-                    word = word.ToLower();
-
-                    if (vectors.ContainsKey(word))
-                    {
-                        vectors[word][d] += 1;
-                    }
-                    else
-                    {
-                        vectors[word] = new int[2];
-                        vectors[word][d] = 1;
-                        //vectors[word][i+1%2] = 0;
+            parseDocument(doc1FilePath, 0, vectors);
+            parseDocument(doc2FilePath, 1, vectors);
 
 
-                    }
-                }
-
-                }
 
             //foreach(KeyValuePair<string, int[]> element in vectors)
             //{
@@ -95,20 +44,72 @@ namespace DocumentDistance
 
             double D1Squared = 0;
             double D2Squared = 0;
-            foreach(string key in vectors.Keys)
+            foreach (string key in vectors.Keys)
             {
                 D1 = vectors[key][0];
-                D2 = vectors[key][1]; 
+                D2 = vectors[key][1];
                 D1xD2 += D1 * D2; //not squared yet
                 D1Squared += D1 * D1; ;
                 D2Squared += D2 * D2;
             }
             double productOfSquares = D1Squared * D2Squared;
-            double quotient =  D1xD2 / (Math.Sqrt(productOfSquares));
+            double quotient = D1xD2 / (Math.Sqrt(productOfSquares));
             double angle = Math.Acos(quotient);
             angle = angle * 180 / Math.PI;
             Console.WriteLine(angle);
             return angle;
+        }
+
+        public static void parseDocument(string path, int docNo, Dictionary<string, int[]> vectors)
+        {
+            string doc = File.ReadAllText(path);
+            int length = doc.Length;
+            string word = "";
+            char letter;
+            for (int i = 0; i < length; i++)
+            {
+                letter = doc[i];
+                if (Char.IsLetterOrDigit(letter))
+                {
+                    word += letter;
+                }
+                else if (word.Length > 0)
+                {
+                    word = word.ToLower();
+
+                    if (vectors.ContainsKey(word))
+                    {
+                        vectors[word][docNo] += 1;
+                    }
+                    else
+                    {
+                        vectors[word] = new int[2];
+                        vectors[word][docNo] = 1;
+                        //vectors[word][i+1%2] = 0;
+
+
+                    }
+                    word = "";
+
+                }
+            }
+            if (word.Length > 0)
+            {
+                word = word.ToLower();
+
+                if (vectors.ContainsKey(word))
+                {
+                    vectors[word][docNo] += 1;
+                }
+                else
+                {
+                    vectors[word] = new int[2];
+                    vectors[word][docNo] = 1;
+                    //vectors[word][i+1%2] = 0;
+
+
+                }
+            }
         }
     }
 }
